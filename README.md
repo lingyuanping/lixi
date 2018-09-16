97e1fca68bdfd7e7aed5fae4f63499df9f5d04e9
lingniker <lingyuanping@outlook.com>
2018-09-16 23:01:46 +0800

    add i18n icons

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 # this is web ui  framework
 ## author : lingniker
 ### creact 2018/09/07 21:58:00
+
+### 解读了element ui 的一个打包输入文件  build-entry.js
+
+* 作用是动态生成 src index.js 文件
+
+* json-template/string modult模块的生成 可以使用模板进行数据的添加
--- build/bin/icon-init.js
+++ build/bin/icon-init.js
@@ -1,21 +1,20 @@
-'use strict'
+'use strict';
 
-var postcss = require('postcss')
-var fs = require('fs')
-var path = require('path')
-var fontFife = fs.readFileSync(path.resolve(__dirname,'../../packages/theme-chalk/src/icon.scss','utf8'))
-var nodes = postcss.parse('fontFile').nodes
-var classList = []
+var postcss = require('postcss');
+var fs = require('fs');
+var path = require('path');
+var fontFile = fs.readFileSync(path.resolve(__dirname, '../../packages/theme-chalk/src/icon.scss'), 'utf8');
+var nodes = postcss.parse(fontFile).nodes;
+var classList = [];
 
+nodes.forEach((node) => {
+  var selector = node.selector || '';
+  var reg = new RegExp(/\.el-icon-([^:]+):before/);
+  var arr = selector.match(reg);
 
-nodes.forEach((node)=>{
-    var selector = node.selector || '';
-    var reg = new RegExp(/\.lx-icon-([^:]+)before/);
-    var arr = selector.match(reg);
-
-    if(arr && arr[i]){
-        classList.push(arr[i]);
-    }
+  if (arr && arr[1]) {
+    classList.push(arr[1]);
+  }
 });
 
-fs.writeFile(path.resolve(__dirname,'../../examples/icon.json'),JSON.stringify(classList),()=>{})
+fs.writeFile(path.resolve(__dirname, '../../examples/icon.json'), JSON.stringify(classList), () => {});
